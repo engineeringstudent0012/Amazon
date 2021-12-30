3c1efccd5c332292808a5e96b7aef14a2ad92c72
engineerngstudent0012 <engineerngstudent0012@gmail.com>
2021-12-30 09:57:20 +0545

    Second Commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports={
+    images:{
+        domains:['links.papareact.com','fakestoreapi.com',"links.papareact.com"]
+    },
+    env:{
+        stripe_public_key : process.env.STRIPE_PUBLIC_KEY
+    }
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/globals.css'
-
 const MyApp = ({ Component, pageProps }) => {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+    return (
+        // Giving all the app access to next auth login
+        <AuthProvider session={pageProps.session}>
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        </AuthProvider>
+)
 }
+
+import {Provider as AuthProvider} from "next-auth/client"
 
 export default MyApp
